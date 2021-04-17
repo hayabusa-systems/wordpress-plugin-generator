97e70ea3b98a3a83b03595a4ee70f3ee3e9a8f54
Hayato Nakamura <hyt.rugby@gmail.com>
2021-04-17 14:34:57 +0900

    コンフィグのチェックとコードジェネレータの呼出まで

--- bin/wordpress-plugin-generator.js
+++ bin/wordpress-plugin-generator.js
@@ -2,5 +2,54 @@
 
 'use strict';
 
+const { exitOverride } = require('commander');
+// import modules
 const program = require('commander');
-program.version('0.0.1', '-v --version').parse(process.argv);
+const fs = require('fs');
+const config = require('../lib/config');
+const generator = require('../lib/generator');
+
+program.version('0.0.1', '-v --version')
+    .option('-d, --debug', 'output extra debugging')
+    .option('-t, --test', 'Test configuration file.')
+    .option('-c,--config-file <type>', 'Specified config file.')
+    .parse(process.argv);
+
+const options = program.opts();
+const debug = options.debug;
+if (debug) {
+  console.log("Debug Mode = ON");
+  console.log("=== Use Option ===");
+  console.log(options);
+  console.log("=== Use Option ===");
+}
+
+let configPath = './config.json';
+if (options.configFile) {
+  configPath = options.configFile;
+  if (debug) {
+    console.log("Switch config file path. Use " + configPath);
+  }
+}
+
+if (!fs.existsSync(configPath)) {
+  console.error('Can not read config file[ ' + configPath + ' ]');
+  process.exit(1);
+}
+
+const configObject = JSON.parse(fs.readFileSync(configPath, 'utf8'));
+if (debug) {
+  console.log('=== Read config file ===');
+  console.log(configObject);
+  console.log('=== Read config file ===');
+}
+if (!config.validate(configPath)) {
+  console.error('Not valid config file.');
+  process.exit(1);
+}
+// テストモードのときはここで終わり
+if (options.test !== undefined && options.test) {
+  process.exit(0);
+}
+
+generator(configPath);
